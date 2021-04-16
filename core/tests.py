795fa548d40a036a46fd9e7c42f0f09f87fe4ab6
Andres Vargas <andres@python3.ninja>
2021-04-16 13:45:32 -0500

    update core

--- core/tests.py
+++ core/tests.py
@@ -5,15 +5,24 @@ from .models import Line, Order
 from .serializers import LineSerializer, OrderSerializer
 
 
+FOOD_LIST = [
+            'Cheese Pizza', 'Hamburger', 'Cheeseburger', 'Bacon Burger', 'Bacon Cheeseburger',
+            'Little Hamburger', 'Little Cheeseburger', 'Little Bacon Burger', 'Little Bacon Cheeseburger',
+            'Veggie Sandwich', 'Cheese Veggie Sandwich', 'Grilled Cheese',
+            'Cheese Dog', 'Bacon Dog', 'Bacon Cheese Dog', 'Pasta'
+]
+
+
 class InitMixin:
     @classmethod
     def setUpTestData(cls):
         seed = Seed.seeder()
-        cls.make_user("u1")
-        cls.make_user("u2")
+        self.u1 = cls.make_user("u1")
+        self.u2 = cls.make_user("u2")
         seed.add_entity(Line, 10)
         seed.add_entity(Order, 2, {'user': lambda x: User.objects.all().order_by("?")[0]})
         seed.execute()
+        self.seed = seed
 
 
 class TestFlow(InitMixin, TestCase):
@@ -24,3 +33,17 @@ class TestFlow(InitMixin, TestCase):
 
     def test_endpoint(self):
         self.get_check_200("/api/orders/")
+        
+        data = {
+            'user': self.u1.id
+            'lines': [
+                {
+                'product_name':     self.seed.faker.random_choice(FOOD_LIST, 1)
+                }
+            ]
+        }
+        assert False, data
+
+        self.post("/api/orders/", data=data)
+
+
